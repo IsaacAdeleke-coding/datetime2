7121962f068b4e1a77803ccb97c6d3020adbe70d
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-12-12 20:25:43 -0800

    Enhance DateTime Utility Suite
    - Add time zone support with daylight saving time adjustments.
    - Implement Zeller's Congruence algorithm to determine the day of the week.
    - Add serialization and deserialization methods for DateTime objects.
    - Include support for multiple serialization formats with error handling.
    - Improve the main file to demonstrate these new features.
    "

---
+++ datetime2/datetime.cpp
@@ -0,0 +1,8 @@
+//
+//  datetime.cpp
+//  datetime2
+//
+//  Created by Isaac Adeleke on 12/12/24.
+//
+
+#include "datetime.hpp"
---
+++ datetime2/datetime.hpp
@@ -0,0 +1,13 @@
+//
+//  datetime.hpp
+//  datetime2
+//
+//  Created by Isaac Adeleke on 12/12/24.
+//
+
+#ifndef datetime_hpp
+#define datetime_hpp
+
+#include <stdio.h>
+
+#endif /* datetime_hpp */
