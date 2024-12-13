d9310c9d3a0b6a1bd41e24652e824ee1aab48bea
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-12-12 20:20:42 -0800

    Initial Commit

---
+++ datetime2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  datetime2
+//
+//  Created by Isaac Adeleke on 12/12/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
