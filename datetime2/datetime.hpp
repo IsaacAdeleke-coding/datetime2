3f13fe9b4a38302c36fd4fcac33f2ba0fae02a15
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-12-13 16:38:15 -0800

    Update datetime.hpp

--- datetime2/datetime.hpp
+++ datetime2/datetime.hpp
@@ -1,13 +1,61 @@
-//
-//  datetime.hpp
-//  datetime2
-//
-//  Created by Isaac Adeleke on 12/12/24.
-//
+#ifndef DATETIME_HPP
+#define DATETIME_HPP
 
-#ifndef datetime_hpp
-#define datetime_hpp
+#include <string>
 
-#include <stdio.h>
+// Base Class: TimeBase
+class TimeBase {
+public:
+    virtual std::string toString() const = 0;
+    virtual ~TimeBase() = default;
+};
 
-#endif /* datetime_hpp */
+// Derived Class: Time
+class Time : public TimeBase {
+private:
+    int hour, minute, second;
+public:
+    Time(int hour, int minute, int second);
+    void addSeconds(int seconds);
+    void addMinutes(int minutes);
+    void addHours(int hours);
+    std::string toString() const override;
+};
+
+// Base Class: DateBase
+class DateBase {
+public:
+    virtual std::string toString() const = 0;
+    virtual std::string dayOfWeek() const = 0;
+    virtual ~DateBase() = default;
+};
+
+// Derived Class: Date
+class Date : public DateBase {
+private:
+    int year, month, day;
+    bool isLeapYear(int year) const;
+    int daysInMonth(int year, int month) const;
+public:
+    Date(int year, int month, int day);
+    void addDays(int days);
+    void addMonths(int months);
+    void addYears(int years);
+    std::string toString() const override;
+    std::string dayOfWeek() const override;
+};
+
+// Derived Class: DateTime
+class DateTime : public Date, public Time {
+private:
+    int timeZoneOffset;
+public:
+    DateTime(int year, int month, int day, int hour, int minute, int second, int timeZoneOffset);
+    void adjustToTimeZone(int newTimeZoneOffset);
+    static int differenceInSeconds(const DateTime& dt1, const DateTime& dt2);
+    std::string serialize() const;
+    static DateTime deserialize(const std::string& serialized);
+    std::string format(const std::string& formatString) const;
+};
+
+#endif // DATETIME_HPP
