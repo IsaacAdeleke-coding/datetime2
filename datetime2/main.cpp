2cc12c2eea8037e433d6fb6dfe6883c83b8a144b
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-12-13 16:44:52 -0800

    Update main.cpp

--- datetime2/main.cpp
+++ datetime2/main.cpp
@@ -5,10 +5,25 @@
 //  Created by Isaac Adeleke on 12/12/24.
 //
 
+#include "datetime.hpp"
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    DateTime dt1(2023, 12, 10, 23, 59, 59, -300); // EST Time Zone
+    DateTime dt2(2023, 12, 9, 12, 30, 0, 0);      // UTC Time Zone
+
+    cout << "DateTime 1: " << dt1.date.toString() << " " << dt1.time.toString() << endl;
+    cout << "DateTime 2: " << dt2.date.toString() << " " << dt2.time.toString() << endl;
+
+    dt1.adjustToTimeZone(0); // Convert to UTC
+    cout << "DateTime 1 (Converted to UTC): " << dt1.date.toString() << " " << dt1.time.toString() << endl;
+
+    cout << "Day of the Week (dt2): " << dt2.date.dayOfWeek() << endl;
+
+    cout << "Serialized dt1: " << dt1.serialize() << endl;
+    DateTime dt3 = DateTime::deserialize("2023-12-10T23:59:59Z-300");
+    cout << "Deserialized dt3: " << dt3.date.toString() << " " << dt3.time.toString() << endl;
+
     return 0;
 }
