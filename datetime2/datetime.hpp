15c9b56ccd302cc7c363de5081d1871f5b064f46
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-12-13 16:41:25 -0800

    Update datetime.hpp

--- datetime2/datetime.hpp
+++ datetime2/datetime.hpp
@@ -3,58 +3,55 @@
 
 #include <string>
 
-// Base Class: TimeBase
-class TimeBase {
+// Base Class: DateTimeBase
+class DateTimeBase {
 public:
     virtual std::string toString() const = 0;
-    virtual ~TimeBase() = default;
+    virtual ~DateTimeBase() = default;
 };
 
 // Derived Class: Time
-class Time : public TimeBase {
-private:
-    int hour, minute, second;
+class Time : public DateTimeBase {
 public:
-    Time(int hour, int minute, int second);
+    int hour, minute, second;
+
+    Time(int hour = 0, int minute = 0, int second = 0);
     void addSeconds(int seconds);
     void addMinutes(int minutes);
     void addHours(int hours);
     std::string toString() const override;
 };
 
-// Base Class: DateBase
-class DateBase {
-public:
-    virtual std::string toString() const = 0;
-    virtual std::string dayOfWeek() const = 0;
-    virtual ~DateBase() = default;
-};
-
 // Derived Class: Date
-class Date : public DateBase {
-private:
+class Date : public DateTimeBase {
+public:
     int year, month, day;
+
+    Date(int year = 1970, int month = 1, int day = 1);
     bool isLeapYear(int year) const;
     int daysInMonth(int year, int month) const;
-public:
-    Date(int year, int month, int day);
     void addDays(int days);
     void addMonths(int months);
     void addYears(int years);
     std::string toString() const override;
-    std::string dayOfWeek() const override;
+    std::string dayOfWeek() const; // Returns the day of the week
 };
 
 // Derived Class: DateTime
 class DateTime : public Date, public Time {
-private:
-    int timeZoneOffset;
 public:
-    DateTime(int year, int month, int day, int hour, int minute, int second, int timeZoneOffset);
+    int timeZoneOffset; // Time zone offset in minutes (e.g., -300 for EST)
+
+    DateTime(int year, int month, int day, int hour, int minute, int second, int timeZoneOffset = 0);
+
     void adjustToTimeZone(int newTimeZoneOffset);
+    void applyDaylightSavingTime(int dstStartMonth, int dstStartDay, int dstEndMonth, int dstEndDay);
+
     static int differenceInSeconds(const DateTime& dt1, const DateTime& dt2);
+
     std::string serialize() const;
     static DateTime deserialize(const std::string& serialized);
+
     std::string format(const std::string& formatString) const;
 };
 
